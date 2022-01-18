43d71848bc2874e45f5441e0ade6304cbe714d6d
Meron Ambelu <48333207+mambelu@users.noreply.github.com>
2022-01-18 12:26:17 -0500

    Add files via upload

---
+++ src/main/java/com/freshCoders/OrganicMarket/OrganicMarketApplication.java
@@ -0,0 +1,13 @@
+package com.freshCoders.OrganicMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganicMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freshCoders/OrganicMarket/OrganicMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshCoders.OrganicMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
